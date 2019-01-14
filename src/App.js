c3fc1d2bfcd5d4c29e9fd08b9fda89bed652758d
keithchasenm <keithchasen89@gmail.com>
2019-01-14 18:34:26 +0200

    auth components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
 
 class App extends Component {
   render() {
@@ -14,6 +17,8 @@ class App extends Component {
                 <Switch>
                     <Route exact path="/" component={Dashboard} />
                     <Route path="/project/:id" component={ProjectDetails} />
+                    <Route path="/signin" component={SignIn} />
+                    <Route path="/signup" component={SignUp} />
                 </Switch>
             </div>
         </BrowserRouter>
