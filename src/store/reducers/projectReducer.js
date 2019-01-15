2245715bee18d8b572b3ab25ddaea5d07a1ec28c
keithchasenm <keithchasen89@gmail.com>
2019-01-15 18:56:26 +0200

    set up project reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+    projects: [
+        {id:'1',title:'Find the Job',content:'If you don\'t find the job then you gonna die'},
+        {id:'2',title:'Buy some champagne and sushi',content:'Only if the job is found'},
+        {id:'3',title:'Learn React',content:'If you wanna be the front end developer'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state
