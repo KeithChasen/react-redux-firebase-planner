d0a2cbb3376120b64377a0f4625f7e5385377f07
keithchasenm <keithchasen89@gmail.com>
2019-02-12 17:02:51 +0200

    tracking auth status in navbar

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -5,19 +5,16 @@ const initState = {
 const authReducer = (state = initState, action) => {
     switch (action.type) {
         case 'LOGIN_ERROR':
-            console.log('Login Error')
             return {
                 ...state,
                 authError: 'Login Failed'
             }
         case 'LOGIN_SUCCESS':
-            console.log('Login Success')
             return {
                 ...state,
                 authError: null
             }
         case 'SIGNOUT_SUCCESS':
-            console.log('Logout Success')
             return state
         default:
             return state
