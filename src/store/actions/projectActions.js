89d22d7a8bea6a6ef23d45851c3a2df8da8c4533
keithchasenm <keithchasen89@gmail.com>
2019-02-12 10:59:04 +0200

    saving project to firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,10 +1,19 @@
-// import {getFirestore} from "redux-firestore/index";
-// import {getFirebase} from "react-redux-firebase/index";
-
 export const createProject = project => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to DB
+        const firestore = getFirestore()
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Keith',
+            authorLastName: 'Chasen',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({type: 'CREATE_PROJECT', project})
+        }).catch((error) => {
+            dispatch({ 'type': 'CREATE_PROJECT_ERROR', error})
+        })
 
-        dispatch({type: 'CREATE_PROJECT', project})
+
     }
 }
