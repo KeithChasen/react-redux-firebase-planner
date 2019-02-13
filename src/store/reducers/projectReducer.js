fc333148e4faa7508ed986b7cce2a85d6ddeecff
keithchasenm <keithchasen89@gmail.com>
2019-02-13 17:46:13 +0200

    order project and notifications

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,9 +1,4 @@
 const initState = {
-    projects: [
-        {id:'1',title:'Find the Job',content:'If you don\'t find the job then you gonna die'},
-        {id:'2',title:'Buy some champagne and sushi',content:'Only if the job is found'},
-        {id:'3',title:'Learn React',content:'If you wanna be the front end developer'}
-    ]
 }
 
 const projectReducer = (state = initState, action) => {
